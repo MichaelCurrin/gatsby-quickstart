e2b353b1655f4723aeb599b8a71996d0921241e6
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-10-28 18:51:54 +0200

    feat: Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+    <h1>Gatsby QuickStart</h1>
+    <p>
+      Hello, world!
+    </p>
+  </div>
 }
