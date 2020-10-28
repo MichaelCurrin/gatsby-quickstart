a0a9183804e5e038e65eac76f22f20204061d729
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-10-28 18:54:17 +0200

    style: Format

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import React from "react"
 
 export default function Home() {
-  return <div>
-    <h1>Gatsby QuickStart</h1>
-    <p>
-      Hello, world!
-    </p>
-  </div>
+  return (
+    <div>
+      <h1>Gatsby QuickStart</h1>
+      <p>Hello, world!</p>
+    </div>
+  )
 }
